3c793ff3e976e63734d36d307f6734802a8f7ac7
sebhilltop@gmail.com <sebhilltop@gmail.com>
2024-08-15 00:05:15 +1200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace roundBtn
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+             CustomButton customButton = new CustomButton();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.System;
+            button1.Location = new Point(261, 258);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            
+            button1.UseVisualStyleBackColor = true;
+            // 
+            //Custom Button
+            
+            customButton.Location = new Point(61, 58);
+            customButton.Name = "click me";
+            customButton.Size = new Size(240, 60);
+            customButton.TabIndex = 0;
+            customButton.Text = "click me";
+            customButton.BorderRadius = 24;
+            customButton.BorderSize = 11;
+            customButton.BorderColor = Color.Black;
+            //
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(customButton);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private CustomButton customButton;
+        
+    }
+}
---
+++ RoundButton.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.Net.Http.Headers;
+using System.ComponentModel;
+
+namespace roundBtn
+{
+    public class CustomButton : Button
+    {   
+        //Fields:
+
+        private int borderSize = 0;
+        private int borderRadius = 24;
+        private Color borderColor = Color.Black;
+
+        [Category("Custom Properties")]
+        public int BorderSize
+        {
+            get { return borderSize; }
+            set 
+            {
+                borderSize = value;
+                this.Invalidate();
+            }
+        }
+        [Category("Custom Properties")]
+        public int BorderRadius
+        {
+            get { return borderRadius; }
+            set
+            {
+                borderRadius = value;
+                this.Invalidate();
+            }
+        }
+        [Category("Custom Properties")]
+        public Color BorderColor
+        {
+            get { return borderColor; }
+            set
+            {
+                borderColor = value;
+                this.Invalidate();
+            }
+        }
+
+
+        //Constructor:
+        public CustomButton()
+        {
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+            this.BackColor = Color.White;
+            this.ForeColor = Color.Blue;
+            this.Size = new Size(240, 60);
+            this.TextAlign = ContentAlignment.MiddleCenter;
+            this.Resize += new EventHandler(Button_Resize);
+        }
+
+        private void Button_Resize(object sender, EventArgs e)
+        {
+            if (borderRadius > this.Height)
+                borderRadius = this.Height;
+        }
+
+        //Methods:
+
+        private GraphicsPath GetFigurePath(Rectangle rect, float radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            float curveSize = radius * 2F;
+
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, curveSize, curveSize, 180, 90);
+            path.AddArc(rect.Right - curveSize, rect.Y, curveSize, curveSize, 270, 90);
+            path.AddArc(rect.Right - curveSize, rect.Bottom - curveSize, curveSize, curveSize, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - curveSize, curveSize, curveSize, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Rectangle rectSurface = this.ClientRectangle;
+            Rectangle rectBorder = Rectangle.Inflate(rectSurface, -borderSize, -borderSize);
+            int smoothSize = 2;
+            if (borderSize > 0)
+                smoothSize = borderSize;
+            if (borderSize > 2) // Rounded Button
+            {
+
+                using (GraphicsPath pathSurface = GetFigurePath(rectSurface, borderRadius))
+                using (GraphicsPath pathBorder = GetFigurePath(rectBorder, borderRadius - borderSize))
+                using (Pen penSurface = new Pen(this.Parent.BackColor, borderSize))
+                using (Pen penBorder = new Pen(borderColor, borderSize))
+                {
+                    e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                    //Button surface:
+                    this.Region = new Region(pathSurface);
+                    //Draw the surface border for HD results:
+                    e.Graphics.DrawPath(penSurface, pathSurface);
+                    // Button Border:
+                    if (borderSize >= 1)
+                        e.Graphics.DrawPath(penBorder, pathBorder);
+                }
+            }
+            else // That is for normal button:
+
+            {
+                e.Graphics.SmoothingMode = SmoothingMode.None;
+                //Button surface:
+                this.Region = new Region(rectSurface);
+                //Button Border:
+                if (borderSize >= 1)
+                {
+                    using (Pen penBorder = new Pen(borderColor, borderSize))
+                    {
+                        penBorder.Alignment = PenAlignment.Inset;
+                        e.Graphics.DrawRectangle(penBorder,0, 0, this.Width -1, this.Height -1);
+                    }
+                    
+                }
+             
+            }
+          
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            this.Parent.BackColorChanged += new EventHandler(Container_BackColorChanged);
+        }
+
+        private void Container_BackColorChanged(object sender, EventArgs e)
+        {
+
+                this.Invalidate();
+        }
+
+    }
+}
