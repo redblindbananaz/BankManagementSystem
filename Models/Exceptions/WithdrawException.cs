8dd821ed5d4f6a892d4153bed37a2e377bca3e22
sebhilltop@gmail.com <sebhilltop@gmail.com>
2024-10-09 13:39:14 +1200

    Creation of the base Withdraw Exception and Unitests file and dependencies

---
+++ Models/Exceptions/WithdrawException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSystem.Models.Exceptions
+{
+    public class WithdrawException : Exception
+    {
+        public WithdrawException(string message) : base(message)
+        {
+        }
+
+        public WithdrawException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
